2e02cf25e731f1fe527b93d75dfa08e9bccad6d8
Hans Chan <hanchai.chc@alibaba-inc.com>
2017-07-18 13:41:16 +0800

    fix: c

--- packages/c/index.js
+++ packages/c/index.js
@@ -1,9 +1,9 @@
-const version = '2.1.0';
+const version = '2.2.0';
 const pkgA = require('webpack-npm-multi-version-pkg-a');
 const pkgB = require('webpack-npm-multi-version-pkg-b');
 
 module.exports = function() {
-  console.log('pkg-c: ' + version + '; require a@^2.0.0 b@^1.2.0');
+  console.log('pkg-c: ' + version + '; require a@^2.1.0 b@^2.1.0');
   pkgA();
   pkgB();
   console.log('==================');
